2b6fe312ec8fdedd4264e09e50c78fefe2e71cfb
Leonid Zelov <112327440+LeoniD994@users.noreply.github.com>
2024-05-21 21:43:17 +0300

    fix

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import'./js/pixabay-api.js';
+import'./js/render-function.js';
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+// Your API key: 44002724-78e4880ab6dd2cf163db4493f
+// Описаний у документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
